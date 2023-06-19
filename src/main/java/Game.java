0afae7a11c67147297d40dbb834c60bdfd382025
ss13 <ssa0618@yandex.ru>
2023-06-19 12:42:56 +0300

    add Hashmap

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,38 +1,34 @@
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    List<Player> players = new ArrayList<>();
-
-    public void register(Player player) {
-        players.add(player);
-    }
+    HashMap<String, PlayerData> players = new HashMap<>();
+    public void register(String name,PlayerData player) {
+        players.put(name, player);    }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = null;
-        Player player2 = null;
-        for (Player player : players) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
+//        PlayerData player1 = null;
+//        PlayerData player2 = null;
+        PlayerData player1 = players.get(playerName1);
+        PlayerData player2 = players.get(playerName2);
+
+            if (player1 == null) {
+                throw new NotRegisteredException(playerName1);
             }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
+            if (player2 == null) {
+                throw new NotRegisteredException(playerName2);
             }
+            if (player1.getStrength() > player2.getStrength()) {
+                return 1;
+            }
+            if (player1.getStrength() < player2.getStrength()) {
+                return 2;
+            }
+            return 0;
         }
-        if (player1 == null) {
-            throw new NotRegisteredException(playerName1);
-        }
-        if (player2 == null) {
-            throw new NotRegisteredException(playerName2);
-        }
-        if (player1.getStrength() > player2.getStrength()) {
-            return 1;
-        }
-        if (player1.getStrength() < player2.getStrength()) {
-            return 2;
-        }
-        return 0;
+
+
     }
 
-}
