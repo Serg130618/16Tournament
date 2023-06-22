81c81a13cb9fbc70d37aa7b96f4c7791d4b3e19e
ss13 <ssa0618@yandex.ru>
2023-06-22 12:02:48 +0300

    Reformat code in main

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,34 +1,37 @@
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    HashMap<String, PlayerData> players = new HashMap<>();
-    public void register(String name,PlayerData player) {
-        players.put(name, player);    }
+    List<PlayerData> players = new ArrayList<>();
+
+    public void register(PlayerData player) {
+        players.add(player);
+    }
 
     public int round(String playerName1, String playerName2) {
-//        PlayerData player1 = null;
-//        PlayerData player2 = null;
-        PlayerData player1 = players.get(playerName1);
-        PlayerData player2 = players.get(playerName2);
-
-            if (player1 == null) {
-                throw new NotRegisteredException(playerName1);
+        PlayerData player1 = null;
+        PlayerData player2 = null;
+        for (PlayerData player : players) {
+            if (player.getName().equals(playerName1)) {
+                player1 = player;
             }
-            if (player2 == null) {
-                throw new NotRegisteredException(playerName2);
+            if (player.getName().equals(playerName2)) {
+                player2 = player;
             }
-            if (player1.getStrength() > player2.getStrength()) {
-                return 1;
-            }
-            if (player1.getStrength() < player2.getStrength()) {
-                return 2;
-            }
-            return 0;
         }
-
-
+        if (player1 == null) {
+            throw new NotRegisteredException(playerName1);
+        }
+        if (player2 == null) {
+            throw new NotRegisteredException(playerName2);
+        }
+        if (player1.getStrength() > player2.getStrength()) {
+            return 1;
+        }
+        if (player1.getStrength() < player2.getStrength()) {
+            return 2;
+        }
+        return 0;
     }
+}
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,52 +1,57 @@
 import org.junit.jupiter.api.Assertions;
-
 import org.junit.jupiter.api.Test;
+
 class GameTest {
     Game game = new Game();
-    PlayerData player1 = new PlayerData(1, "Ivan", 70);
-    PlayerData player2 = new PlayerData(2, "Serg", 65);
-
     @Test
     public void testWhenFirstWin() {
+        PlayerData player1 = new PlayerData(1, "Ivan", 70);
+        PlayerData player2 = new PlayerData(2, "Serg", 65);
 
-        game.register("Ivan", player1);
-        game.register("Serg", player2);
+        game.register(player1);
+        game.register(player2);
 
         int actual = game.round("Ivan", "Serg");
         int expected = 1;
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
     public void testWhenWin() {
+        PlayerData player1 = new PlayerData(1, "Ivan", 70);
+        PlayerData player2 = new PlayerData(2, "Serg", 75);
 
-        game.register("Ivan", player1);
-        game.register("Serg", player2);
+        game.register(player1);
+        game.register(player2);
 
         int actual = game.round("Serg", "Ivan");
-        int expected = 2;
+        int expected = 1;
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
     public void testDidNotWin() {
+        PlayerData player1 = new PlayerData(1, "Ivan", 70);
+        PlayerData player2 = new PlayerData(2, "Serg", 70);
 
-        game.register("Ivan", player1);
-        game.register("Serg", player2);
+        game.register(player1);
+        game.register(player2);
 
-        int actual = game.round( "Serg", "Serg");
+        int actual = game.round("Ivan", "Serg");
         int expected = 0;
 
-        Assertions.assertEquals( expected, actual);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
     public void testNotRegistered() {
+        PlayerData player1 = new PlayerData(1, "Ivan", 70);
+        PlayerData player2 = new PlayerData(2, "Serg", 70);
 
-        game.register("Ivan", player1);
-        game.register("Serg", player2);
+        game.register(player1);
+        game.register(player2);
         Assertions.assertThrows(NotRegisteredException.class, () -> {
             game.round("Maks", "Serg");
         });
@@ -54,14 +59,16 @@ class GameTest {
 
     @Test
     public void testWhenSecondWin() {
+        PlayerData player1 = new PlayerData(1, "Ivan", 70);
+        PlayerData player2 = new PlayerData(2, "Serg", 75);
 
-        game.register("Ivan", player1);
-        game.register("Serg", player2);
+        game.register(player1);
+        game.register(player2);
 
-        int actual = game.round("Serg", "Ivan");
+        int actual = game.round("Ivan", "Serg");
         int expected = 2;
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(actual, expected);
     }
 
 }
